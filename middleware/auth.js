44d4ccd5574b7acb997217e3daf0a780e7ad49a9
abbas <86982819+abbassiriyus@users.noreply.github.com>
2023-11-19 13:48:10 +0500

    wqer
    qwer

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,12 +2,12 @@ const jwt = require('jsonwebtoken');
 
 function verifyToken(req, res, next) {
   const token = req.headers.authorization;
-
+console.log(token);
   if (!token) {
     return res.status(401).json({ error: 'No token provided' });
   }
 
-  jwt.verify(token, 'your_secret_key', (err, decoded) => {
+  jwt.verify(token.slice(7), 'your_secret_key', (err, decoded) => {
     if (err) {
       return res.status(403).json({ error: 'Failed to authenticate token' });
     }
